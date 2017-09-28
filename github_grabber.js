1e96e24119fc5fdb59a5c65f00b5c9dc7a147666
Brandon Tat <brandontat7@gmail.com>
2017-09-28 13:48:55 -0700

    grab username through post request

--- github_grabber.js
+++ github_grabber.js
@@ -1,10 +1,19 @@
-// const fs = require('fs');
-// const http = require('http');
-// const querystring = require('querystring');
-//
-// const server = http.createServer((req, res) => {
-//   res.write('hello b');
-//   res.end();
-// });
-//
-// server.listen(8000, () => console.log("I'm listening on port 8000"));
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const queryString = require('querystring');
+
+const githubServer = http.createServer((req, res) => {
+  if (req.method === 'POST') {
+    let body = '';
+    req.on('data', d => {
+      body += d;
+    });
+    req.on('end', () => {
+      const username = queryString.parse(body).username;
+      res.end(username);
+    });
+  }
+});
+
+githubServer.listen(8000, () => console.log('Listening on port 8000'));
