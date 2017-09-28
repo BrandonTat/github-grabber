058ac13379f44ff31b47ae23db74796837438432
Brandon Tat <brandontat7@gmail.com>
2017-09-28 14:21:13 -0700

    grab all repos from a user given their username

--- github_grabber.js
+++ github_grabber.js
@@ -3,6 +3,16 @@ const http = require('http');
 const https = require('https');
 const queryString = require('querystring');
 
+function buildOptionsObj (username) {
+  return {
+    hostname: `api.github.com`,
+    path: `/users/${username}/repos`,
+    headers: {
+      'User-Agent': 'github-grabber'
+    }
+  };
+}
+
 const githubServer = http.createServer((req, res) => {
   if (req.method === 'POST') {
     let body = '';
@@ -11,7 +21,20 @@ const githubServer = http.createServer((req, res) => {
     });
     req.on('end', () => {
       const username = queryString.parse(body).username;
-      res.end(username);
+      const ws = fs.createWriteStream(`./${username}_repos.txt`);
+      const opts = buildOptionsObj(username);
+
+      https.get(opts, (dataStream) => {
+        let repoData = "";
+        dataStream.on('data', chunk => { repoData += chunk });
+        dataStream.on('end', () => {
+          const repos = JSON.parse(repoData).map(repo => {
+            return `Repo: ${repo.name}`;
+          }).join('\n');
+          ws.write(repos);
+          res.end(repos);
+        });
+      });
     });
   }
 });
